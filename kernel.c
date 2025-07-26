6f9c2a8409d232abd0f634e4c6a17f19afa0503c
Artem Vasiukov <vasartvas2@gmail.com>
2025-07-26 20:34:04 +0300

    exception handling implemented

--- kernel.c
+++ kernel.c
@@ -1,19 +1,15 @@
-#include <stdbool.h>
-#include <stddef.h>
-#include <stdint.h>
-
-#if defined(__linux__)
-#error "You are not using a cross-compiler, you will most certainly run into trouble"
-#endif
-
 #if !defined(__i386__)
-#error "This tutorial needs to be compiled with a ix86-elf compiler"
+#error "The kernel shall be compiled with a ix86-elf compiler"
 #endif
 
 #include "vgaBufferTerminal/terminal.h"
+#include "interrupts/interruptDescriptorTable/idt_initialization.h"
 
 void kernel_main(void) {
 	terminal_initialize();
+	terminal_print_new_prompt();
 
-	terminal_print_prompt();
+	initialize_idt();
+
+	while(1);
 }
